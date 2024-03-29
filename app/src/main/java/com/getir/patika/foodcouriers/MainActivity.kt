3997fa531c0d5e92701cd5abd61728c95cc2ba2c
selensonmez <selensonmez2001@gmail.com>
2024-03-29 20:27:37 +0300

    navigate to map view

--- app/src/main/java/com/getir/patika/foodcouriers/MainActivity.kt
+++ app/src/main/java/com/getir/patika/foodcouriers/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.getir.patika.foodcouriers
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
@@ -11,7 +13,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var tabLayout: TabLayout
     private lateinit var viewPager2: ViewPager2
     private lateinit var pagerAdapter: PagerAdapter
-
+    private lateinit var btnShowMap: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account)
@@ -34,6 +36,13 @@ class MainActivity : AppCompatActivity() {
              }
 
         }.attach()
+
+        // Initialize the button
+        btnShowMap = findViewById(R.id.btn_show_map)
+        // Set OnClickListener to start MapActivity when the button is clicked
+        btnShowMap.setOnClickListener {
+            startActivity(Intent(this, MapActivity::class.java))
+        }
 
     }
 }
